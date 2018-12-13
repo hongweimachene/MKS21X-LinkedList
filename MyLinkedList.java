a0bea93f3438c8469a8209235763a6bfaa950aa5
hongweimachene <hchen00@stuy.edu>
2018-12-13 10:02:12 -0500

    put node in linked list

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,30 @@
 public class MyLinkedList{
+  private class Node{
+    private Integer data;
+    private Node next,prev;
+    public Node(int val) {
+      data = val;
+    }
+    public Node next() {
+      return next;
+    }
+    public Node prev() {
+      return prev;
+    }
+    public Integer value() {
+      return data;
+    }
+    public void setNext(Node next) {
+      this.next = next;
+    }
+    public void setPrev(Node prev) {
+      this.prev = prev;
+    }
+    public void set(int val) {
+      this.data = val;
+    }
+
+  }
   private int size;
   private Node start,end;
 
