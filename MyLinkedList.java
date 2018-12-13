4c946d6954945c25eb72c78fd55757322ab37c87
hongweimachene <hchen00@stuy.edu>
2018-12-12 23:20:42 -0500

    started classes

---
+++ MyLinkedList.java
@@ -0,0 +1,19 @@
+public class MyLinkedList{
+  private int size;
+  private Node start,end;
+
+  public MyLinkedList(){
+    start = null;
+    end = null;
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(int value){
+    if (size == 0) {
+      start = new Node(value,)
+    }
+  }
+  public String toString();
+}
---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node{
+  private int data;
+  private Node next,prev;
+  public Node(int val, Node next, Node prev) {
+    data = val;
+    this.next = next;
+    this.prev = prev;
+  }
+  
+}
